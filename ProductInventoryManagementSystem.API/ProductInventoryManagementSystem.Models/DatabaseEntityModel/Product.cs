421058ded84ec5e01453905347a052d8afb4fa1d
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-02-23 17:05:38 +0530

    feat: product entity

---
+++ ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.Models/DatabaseEntityModel/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductInventoryManagementSystem.Model.DatabaseEntityModel
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid CreatedBy { get; set; }
+    }
+}
