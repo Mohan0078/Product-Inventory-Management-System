438130d732b17aa952d60de8fe835289108ab221
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-02-23 17:12:20 +0530

    feat: inventory entity

---
+++ ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.Models/DatabaseEntityModel/Inventory.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductInventoryManagementSystem.Model.DatabaseEntityModel
+{
+    public class Inventory
+    {
+        public Guid InventoryId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string WareHouseLocation { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
