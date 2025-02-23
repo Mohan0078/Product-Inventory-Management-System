38adb1460635ff3e09335c0624e5b86ed24e4085
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-02-23 17:27:09 +0530

    feat: role entity

---
+++ ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.Models/DatabaseEntityModel/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementSystem.Model.DatabaseEntityModel
+{
+    public class Role
+    {
+      public Guid RoleId { get; set; }
+      public string RoleName { get; set; }
+      public string RoleType { get; set; }
+      public DateTime CreatedOn { get; set; }
+      public Guid CreatedBy { get; set; }
+    }
+}
