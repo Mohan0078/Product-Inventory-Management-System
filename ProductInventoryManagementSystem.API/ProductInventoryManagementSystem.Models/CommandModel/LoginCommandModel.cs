ebbb72fecc425021f767061f33482a988a110b4f
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-02-23 15:42:58 +0530

    feat: login command model

---
+++ ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.Models/CommandModel/LoginCommandModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementSystem.Model.CommandModel
+{
+    public class LoginCommandModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
