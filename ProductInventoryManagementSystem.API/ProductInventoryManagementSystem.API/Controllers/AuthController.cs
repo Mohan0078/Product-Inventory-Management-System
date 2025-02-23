c450126f497d562f62c23b8aab300cb472d085d3
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-02-23 15:45:44 +0530

    feat: auth controller

---
+++ ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductInventoryManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
