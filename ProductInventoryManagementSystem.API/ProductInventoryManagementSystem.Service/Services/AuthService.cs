4336c8170fabdbdb58770dc354a9b07ea0895c7e
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-02-23 23:44:44 +0530

    feat: do login

--- ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.Service/Services/AuthService.cs
+++ ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.Service/Services/AuthService.cs
@@ -1,15 +1,34 @@
-﻿using ProductInventoryManagementSystem.Model.CommandModel;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductInventoryManagementSystem.Model.CommandModel;
+using ProductInventoryManagementSystem.Model.DatabaseEntityModel;
 using ProductInventoryManagementSystem.Service.Interfaces;
 
 namespace ProductInventoryManagementSystem.Service.Services
 {
     public class AuthService : IAuthService
     {
+		private readonly ProductInventoryManagementDbContext _context;
+        public AuthService(ProductInventoryManagementDbContext context)
+		{
+          _context = context;
+		}
+		/// <summary>
+		/// Taken email and password and return list of roles assigned to a user when login successful
+		/// </summary>
+		/// <param name="loginCommandModel"></param>
+		/// <returns></returns>
         public async Task<List<string>> DoLogin(LoginCommandModel loginCommandModel)
         {
 			try
 			{
-                return new List<string>();
+				var user = await _context.Users.Include(x => x.Roles)
+					          .FirstOrDefaultAsync(x => x.Email == loginCommandModel.Email);
+
+				if(user == null)
+				{
+					throw new Exception("User not exists!");
+				}
+				return user.Roles.Select(x => x.RoleName).ToList();
 			}
 			catch (Exception)
 			{
