4631d450a6ad0a7227abdb2b0bbbd7f391c960db
Mohan Patel <mohanpatel.mp73@gmail.com>
2025-02-23 17:20:10 +0530

    feat: user entity

---
+++ ProductInventoryManagementSystem.API/ProductInventoryManagementSystem.Models/DatabaseEntityModel/User.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductInventoryManagementSystem.Model.DatabaseEntityModel
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
